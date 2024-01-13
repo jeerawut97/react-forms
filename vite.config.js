2b4bb6e43fa9d037b4f2a81d8279607ace845800
o_O <77603903+jeerawut97@users.noreply.github.com>
2024-01-13 13:54:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/jeerawut97/react-forms",
+  base: "https://github.com/jeerawut97/react-forms.github.io",
   plugins: [react()],
 })
